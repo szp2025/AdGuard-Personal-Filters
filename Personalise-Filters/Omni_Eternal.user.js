e980e5360682154c0a05ad741bae1bc90c9471a4
szp2025 <szymanskipawel1441@gmail.com>
2026-03-30 19:24:12 +0200

    Fix script name formatting in Omni_Eternal.user.js

--- Personalise-Filters/Omni_Eternal.user.js
+++ Personalise-Filters/Omni_Eternal.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name        he Omni-Protocol: Nebula Apex (v13.0.0) Interstellar Void.
+// @name         The Omni-Protocol: Nebula Apex (v13.0.0) Interstellar Void.
 // @version      v13.0.0-APEX
 // @description  Apex Logic Layer v2026.V11. Deep Spoofing, Font-Masking, WebRTC Block & Neural Noise.
 // @author       szp2025 & Gemini AI (Collaborative Cyber-Intelligence)
