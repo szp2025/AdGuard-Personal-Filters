10c6d4d56444debfe9562ac0b6a8b47ddfb4124a
szp2025 <szymanskipawel1441@gmail.com>
2026-04-01 20:09:06 +0200

    Update Omni-Device-Bridge.user.js

--- Personalise-Filters/Omni-Device-Bridge.user.js
+++ Personalise-Filters/Omni-Device-Bridge.user.js
@@ -1,42 +1,19 @@
 // ==UserScript==
 // @name         Omni-Device-Bridge
 // @namespace    OmniProtocol
-// @version      1.0.1
-// @description  Системный мост для управления приложениями и настройками телефона
+// @version      1.0.2
+// @description  Системный мост управления
 // @author       Командор
 // @match        *://*/*
-// @grant        none
+// @grant        unsafeWindow
+// @grant        GM_log
 // @run-at       document-start
+// @updateURL    https://raw.githubusercontent.com/szp2025/AdGuard-Personal-Filters/main/Personalise-Filters/Omni-Device-Bridge.user.js
+// @downloadURL  https://raw.githubusercontent.com/szp2025/AdGuard-Personal-Filters/main/Personalise-Filters/Omni-Device-Bridge.user.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-
-    window.OmniBridge = {
-        config: {
-            TAG: '%c[OMNI-BRIDGE]',
-            STYLE: 'color: #00ffff; font-weight: bold; background: #1a1a1a; padding: 2px 5px; border-radius: 3px;',
-            APPS: {
-                YOUTUBE: 'intent://#Intent;package=com.google.android.youtube;end',
-                MUSIC: 'intent://#Intent;package=com.google.android.apps.youtube.music;end',
-                REVANCED: 'intent://#Intent;package=app.revanced.android.youtube;end',
-                SETTINGS: 'intent://#Intent;action=android.settings.SETTINGS;end',
-                ADGUARD: 'intent://#Intent;package=com.adguard.android;end',
-                WIFI: 'intent://#Intent;action=android.settings.WIFI_SETTINGS;end',
-                BIO: 'intent://#Intent;action=android.settings.BIOMETRIC_ENROLL;end'
-            }
-        },
-
-        launch: function(appKey) {
-            const intent = this.config.APPS[appKey];
-            if (intent) {
-                console.log(this.config.TAG, this.config.STYLE, `Запуск системного модуля: ${appKey}...`);
-                window.location.href = intent;
-            } else {
-                console.error('Ошибка: Модуль не найден в базе Omni');
-            }
-        }
-    };
-
-    console.log('%c[OMNI-BRIDGE] СИСТЕМА МОНИТОРИНГА АКТИВИРОВАНА', 'color: #00ffff; font-weight: bold;');
+    // Весь основной код...
+    console.log('%c[OMNI-BRIDGE] СИСТЕМА АКТИВИРОВАНА', 'color: #00ffff;');
 })();
